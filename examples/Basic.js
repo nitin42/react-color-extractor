067939876fcd804ac1d9c928b541bafe6e3e6f8d
Nitin <tulswani19@gmail.com>
2018-08-26 23:28:16 +0530

    refactor the mess

---
+++ examples/Basic.js
@@ -0,0 +1,61 @@
+import React from "react";
+import ColorExtractor from "../src/ColorExtractor";
+
+const IMAGE =
+  "https://images.unsplash.com/photo-1525489196064-0752fa4e16f2?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=d39007f42b1568e412a65313cdfdc63b&auto=format&fit=crop&w=1350&q=80";
+
+const IMAGE_STYLES = { width: 500, height: 500 };
+
+const SWATCHES_STYLES = {
+  marginTop: 20,
+  display: "grid",
+  gridTemplateColumns: "repeat(12, 1fr)",
+  gridGap: "2px"
+};
+
+const Swatches = props => (
+  <div style={SWATCHES_STYLES}>{props.renderSwatches("rgb")}</div>
+);
+
+const WithoutChildren = props => (
+  <React.Fragment>
+    <img id="image" src={IMAGE} style={IMAGE_STYLES} />
+    <ColorExtractor imgId="image" getColors={props.getColors} />
+  </React.Fragment>
+);
+
+class App extends React.Component {
+  state = { colors: [] };
+
+  renderSwatches = type => {
+    const { colors } = this.state;
+
+    return colors.map((color, id) => {
+      return (
+        <div
+          key={id++}
+          style={{
+            backgroundColor:
+              Array.isArray(color) && type === "rgb"
+                ? `rgb(${color[0]}, ${color[1]}, ${color[2]})`
+                : color,
+            width: 100,
+            height: 100
+          }}
+        />
+      );
+    });
+  };
+
+  getColors = ({ colors }) =>
+    this.setState(state => ({ colors: [...state.colors, ...colors] }));
+
+  render() {
+    return (
+      <React.Fragment>
+        <WithoutChildren getColors={this.getColors} />
+        <Swatches renderSwatches={this.renderSwatches} />
+      </React.Fragment>
+    );
+  }
+}
--- src/ColorExtractor.js
+++ src/ColorExtractor.js
@@ -1,31 +1,33 @@
 // @flow
 
-import * as React from 'react'
-import Vibrant from 'node-vibrant'
+import * as React from "react";
+import Vibrant from "node-vibrant";
 
-// Image can also be served by CORS enabled (optional) server
-type HTTPSLink = string
+// Image can also be served remotely (CORS enabled server)
+type HTTPSLink = string;
 
 // Or it can also be an image element, or null (default)
-type Image = HTMLElement | HTTPSLink | null
+type Image = HTMLElement | HTTPSLink | null;
 
+// ColorExtractor component props
 type Props = {
-	// Invoked when there is any error other than CORS or DOM
-	onError: (err: Object) => void,
-	// Main callback which is invoked with all the colors extracted from the image
-	getColors: (data: { colors: Array<string | number> }) => {},
-	// Color format RGB
-	rgb: boolean,
-	// Color format HSL
-	hsl: boolean,
-	// Get the image ref through image element id
-	imgId: string,
-	// Get the image ref through image element className
-	cName: string,
-	// Directly extract the colors from the image specified by 'src' prop
-	src: string,
-	children?: React.Node,
-}
+  // Invoked when there is an error other than CORS or DOM
+  onError: (err: Object) => void,
+  // Main callback which is invoked with all the colors extracted from the image (hex, rgb or hsl)
+  getColors: (data: { colors: Array<string | number> }) => {},
+  // Color format RGB
+  rgb: boolean,
+  // Color format HSL
+  hsl: boolean,
+  // Get the image ref through selector id
+  imgId: string,
+  // Get the image ref through selector className
+  cName: string,
+  // Directly extract the colors from the src
+  src: string,
+  // Optional wrap the image from which the colors will be extracted
+  children?: React.Node
+};
 
 // ColorExtractor - A React component which extracts the colors (hex / rgb / hsl) from an image. The image can be a local or served with CORS enabled.
 
@@ -53,121 +55,141 @@ type Props = {
  */
 
 class ColorExtractor extends React.Component<Props, void> {
-	static defaultProps = {
-		onError: (err: Object) => {},
-		getColors: (data: { colors: Array<number | string> }) => {},
-		rgb: false,
-		hsl: false,
-		hex: true,
-		imgId: null,
-		src: null,
-		cName: null,
-	}
+  static defaultProps = {
+    onError: (err: Object) => {},
+    getColors: (data: { colors: Array<number | string> }) => {},
+    rgb: false,
+    hsl: false,
+    hex: true,
+    imgId: null,
+    src: null,
+    cName: null
+  };
 
-	componentDidMount() {
-		let image = null
+  // Utilities for dealing with color conversions
 
-		// Check if the src is path or an image element. If it's an image element, directly
-		// use Vibrant constructor else if it's an image path, create an hidden image element and extract colors,
-		// or, if it's an image element or id or className, get the element ref.
+  static rgbToHex = Vibrant.Util.rgbToHex;
 
-		if (this.props.imgId) {
-			image = document.getElementById(this.props.imgId)
+  static hexToRgb = Vibrant.Util.hexToRgb;
 
-			if (!image) {
-				throw new Error(`Couldn't find the image with the id "${this.props.imgId}" in the dom tree.`)
-			}
+  static rgbToHsl = Vibrant.Util.rgbToHsl;
 
-			// Check whether it's image element
-			this.isImage(image, this.props.imgId)
-		} else if (this.props.children) {
-			// We assume the children count is one, so we directly access the property props
-			// $FlowFixMe
-			image = this.props.children.props.src
-		} else if (this.props.src) {
-			image = this.props.src
-		} else if (this.props.cName) {
-			image = document.querySelector(`.${this.props.cName}`)
-		}
+  static hslToRgb = Vibrant.Util.hslToRgb;
 
-		this.parseImage(image, this.props)
-	}
+  componentDidMount() {
+    let image = null;
 
-	// Parse the image and extract the colors
-	parseImage = (image: Image, props: Props) => {
-		Vibrant.from(image)
-			.getSwatches()
-			.then(swatches => props.getColors({ colors: this.getColorsFromSwatches(swatches, props) }))
-			.catch(error => {
-				if (error) {
-					// This error is mainly due to CORS issue. So we retry again by using the default image class, but if still there is any error, we bail out there
-					this.useDefaultImageClass(image, props)
-				}
-			})
-	}
+    // Check if the src is path or an image element. If it's an image element, directly
+    // use Vibrant constructor else if it's an image path, create an hidden image element and extract colors,
+    // or, if it's an image element or id or className, get the element ref.
 
-	// Get the array of colors from swatches
-	getColorsFromSwatches = (swatches: Object, props: Props) => {
-		const buffer = []
+    if (this.props.imgId) {
+      image = document.getElementById(this.props.imgId);
 
-		for (let swatch in swatches) {
-			if (swatches.hasOwnProperty(swatch) && swatches[swatch]) {
-				if (props.rgb) {
-					buffer.push(swatches[swatch].getRgb())
-				} else if (props.hsl) {
-					buffer.push(swatches[swatch].getHsl())
-				} else {
-					buffer.push(swatches[swatch].getHex())
-				}
-			}
-		}
+      if (!image) {
+        throw new Error(
+          `Couldn't find the image with the id "${
+            this.props.imgId
+          }" in the dom tree.`
+        );
+      }
 
-		return buffer
-	}
+      if (image.tagName !== "IMG") {
+        throw new Error(
+          `Expected the element with id "${this.props.imgId}" to be an image.`
+        );
+      }
+    } else if (this.props.children) {
+      // We assume the children count is one, so we directly access the props
+      // $FlowFixMe
+      image = this.props.children.props.src;
+    } else if (this.props.src) {
+      image = this.props.src;
+    } else if (this.props.cName) {
+      image = document.querySelector(`.${this.props.cName}`);
+    }
 
-	isImage = (image: Image, id: string) => {
-		// $FlowFixMe
-		if (image.tagName !== 'IMG') {
-			throw new Error(`Expected the element with id "${id}" to be an image.`)
-		}
-	}
+    this.parseImage(image, this.props);
+  }
 
-	useDefaultImageClass = (image: Image, props: Props) => {
-		// $FlowFixMe
-		new Vibrant.DefaultOpts.ImageClass().load(image.src).then(data => {
-			if (data.image) {
-				const colors = []
+  // Parse the image and extract the colors
+  parseImage = (image: Image, props: Props) => {
+    Vibrant.from(image)
+      .getSwatches()
+      .then(swatches =>
+        props.getColors({ colors: this.getColorsFromSwatches(swatches, props) })
+      )
+      .catch(error => {
+        if (error) {
+          // This error is mainly due to CORS issue. So we retry again by using the default image class, but if still there is any error, we bail out there
+          this.useDefaultImageClass(image, props);
+        }
+      });
+  };
 
-				Vibrant.from(data.image)
-					.getSwatches()
-					.then(swatches => props.getColors({ colors: this.getColorsFromSwatches(swatches, props) }))
-					.catch(error => {
-						if (error) {
-							props.onError(error)
-						}
-					})
-			}
-		})
-	}
+  // Get the array of colors from swatches
+  getColorsFromSwatches = (swatches: Object, props: Props) => {
+    const buffer = [];
 
-	render(): ?React.Node {
-		const count = React.Children.count(this.props.children)
+    for (let swatch in swatches) {
+      if (swatches.hasOwnProperty(swatch) && swatches[swatch]) {
+        if (props.rgb) {
+          buffer.push(swatches[swatch].getRgb());
+        } else if (props.hsl) {
+          buffer.push(swatches[swatch].getHsl());
+        } else {
+          buffer.push(swatches[swatch].getHex());
+        }
+      }
+    }
 
-		// We don't handle multiple images at the moment, sorry!
-		if (count > 1) {
-			throw new Error('Expected only one image element.')
-		} else if (count === 1) {
-			// Children should be an image element
-			// $FlowFixMe
-			if (this.props.children.type === 'img') {
-				return this.props.children
-			} else {
-				throw new Error(`Expected children to be an image element but instead got a "${this.props.children.type}"`)
-			}
-		} else {
-			return null
-		}
-	}
+    return buffer;
+  };
+
+  useDefaultImageClass = (image: Image, props: Props) => {
+    // $FlowFixMe
+    new Vibrant.DefaultOpts.ImageClass().load(image.src).then(data => {
+      if (data.image) {
+        const colors = [];
+
+        Vibrant.from(data.image)
+          .getSwatches()
+          .then(swatches =>
+            props.getColors({
+              colors: this.getColorsFromSwatches(swatches, props)
+            })
+          )
+          .catch(error => {
+            if (error) {
+              props.onError(error);
+            }
+          });
+      }
+    });
+  };
+
+  render(): ?React.Node {
+    const length = React.Children.count(this.props.children);
+
+    // We don't handle multiple images at the moment, sorry!
+    if (length > 1) {
+      throw new Error("Expected only one image element.");
+    } else if (length === 1) {
+      // Children should be an image element
+      // $FlowFixMe
+      if (this.props.children.type === "img") {
+        return this.props.children;
+      } else {
+        throw new Error(
+          `Expected children to be an image element but instead got a "${
+            this.props.children.type
+          }"`
+        );
+      }
+    } else {
+      return null;
+    }
+  }
 }
 
-export default ColorExtractor
+export default ColorExtractor;
